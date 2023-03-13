2b308fa24a965bc2d154fb0088a415a994bcf345
Andreas Barbesgaard <Abarbesgaard@gmail.com>
2023-03-13 15:08:26 +0100

    all done

--- Disaheim/Disaheim/Amulet.cs
+++ Disaheim/Disaheim/Amulet.cs
@@ -7,62 +7,42 @@ using System.Threading.Tasks;
 namespace Disaheim
 {   
     public class Amulet
-    {
-        //public string ItemId { get; set; }
-        //public string Design { get; set; }
-       // public Level Quality { get; set; }
-
+    {      
         private string itemId;
-
         public string ItemId
         {
             get { return itemId; }
             set { itemId = value; }
         }
-
         private string design;
-
         public string Design
         {
             get { return design; }
             set { design = value; }
         }
-
         private Level quality;
-
         public Level Quality
         {
-            get { return Quality; }
-            set { Quality = value; }
+            get { return quality; }
+            set { quality = value; }
         }
-
-
         public Amulet(string ItemId, Level Quality, string Design) 
         {
             this.itemId = ItemId;
             this.quality = Quality;
-            this.design = Design;
+            this.design = Design;           
         }
-
-
         public Amulet(string ItemId) :
             this(ItemId, Level.medium,"")
-        { 
-            //this.itemId = ItemId;
-            //quality = Level.medium;
+        {         
         }
         public Amulet(string ItemId, Level Quality) : 
             this(ItemId, Quality,"")
-        { 
-            //this.itemId = ItemId;
-            //this.quality = Quality;
+        {        
         }
-
         public override string ToString()
-        {
-            //"ItemId: 11, Quality: medium, Design: "
+        {          
             return $"ItemId: {itemId}, Quality: {quality}, Design: {design}";
         }
-
     }
 }
